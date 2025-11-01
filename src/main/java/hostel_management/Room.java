d7641c8270724c2253cb09543cec91b99eebca2f
SyedBilal61 <bilalkazmi61.bk@gmail.com>
2025-11-02 00:42:24 +0100

    write the second test in RoomTest.java and make it pass

--- src/main/java/hostel_management/Room.java
+++ src/main/java/hostel_management/Room.java
@@ -3,6 +3,9 @@ package hostel_management;
 public class Room {
 	private String roomnumber;
 	private boolean available = true;
+	private String tenant;
+	
+	
 	
 	public Room(String roomnumber) {
 		this.roomnumber = roomnumber;
@@ -10,4 +13,14 @@ public class Room {
 	public boolean isAvailable() {
 		return available;
 	}
+	
+	public void assignTenant (String tenantName) {
+		if (!available) {
+			throw new IllegalStateException ("Room already occupied");
+		}
+		this.tenant=tenantName;
+		this.available= false;
+		
+	}
+	
 }
--- src/test/java/hostel_management/RoomTest.java
+++ src/test/java/hostel_management/RoomTest.java
@@ -3,10 +3,17 @@ package hostel_management;
 import org.junit.Test;
 import static org.junit.Assert.*;
 public class RoomTest {
-
-	@Test
+	// Room is intially Availble 
+	@Test 
 	public void testRoomInitiallyAvailable() {
 		Room room = new Room("A1");
 		assertTrue(room.isAvailable());
+	}
+	//Assign room and mark room as occupied 
+	@Test
+	public void testAssignRoom() {
+		Room room = new Room("A1");
+		room.assignTenant("Ali");
+		assertFalse(room.isAvailable());
 	}
 }
