aadd5b8c3d3276424c3b0c60e5cd41dd469bd12e
SyedBilal61 <bilalkazmi61.bk@gmail.com>
2025-11-02 00:23:11 +0100

    write the Room.java and make the test Pass

--- src/main/java/hostel_management/Room.java
+++ src/main/java/hostel_management/Room.java
@@ -1,5 +1,13 @@
 package hostel_management;
 
 public class Room {
-
+	private String roomnumber;
+	private boolean available = true;
+	
+	public Room(String roomnumber) {
+		this.roomnumber = roomnumber;
+	}
+	public boolean isAvailable() {
+		return available;
+	}
 }
