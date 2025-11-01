5db679e31fe7ac09af6cada5842701ab41b75eda
SyedBilal61 <bilalkazmi61.bk@gmail.com>
2025-11-02 00:14:44 +0100

    Add Dependency for junit4 and add a test in RoomTest.java

--- src/test/java/hostel_management/RoomTest.java
+++ src/test/java/hostel_management/RoomTest.java
@@ -1,7 +1,12 @@
 package hostel_management;
 
 import org.junit.Test;
-import static org.junit.Assert.*
+import static org.junit.Assert.*;
 public class RoomTest {
 
+	@Test
+	public void testRoomInitiallyAvailable() {
+		Room room = new Room("A1");
+		assertTrue(room.isAvailable());
+	}
 }
