3439d735f5990a45d8754e7043049e5f849f8bfe
SyedBilal61 <bilalkazmi61.bk@gmail.com>
2025-11-11 18:58:20 +0100

    workflow for integration testing for using test containers

---
+++ src/it/java/com/hostel/repository/mongo/ApartmentMongoRepositoryTestContainersIT.java
@@ -0,0 +1,63 @@
+package com.hostel.repository.mongo;
+
+import com.mongodb.MongoClient;
+import com.mongodb.ServerAddress;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import org.bson.Document;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.ClassRule;
+import org.junit.Test;
+import org.testcontainers.containers.GenericContainer;
+
+import static org.junit.Assert.assertNotNull;
+
+public class ApartmentMongoRepositoryTestContainersIT {
+
+	// Start a temporary MongoDB container for this test
+    
+	
+	@SuppressWarnings("rawtypes")
+    @ClassRule
+    public static final GenericContainer mongo =
+            new GenericContainer<>("mongo:4.4.3")
+                    .withExposedPorts(27017);
+
+    private MongoClient client;
+    private MongoDatabase database;
+    private MongoCollection<Document> apartmentCollection;
+
+    @Before
+    public void setup() {
+        // Connect to the MongoDB container
+        client = new MongoClient(
+                new ServerAddress(
+                        mongo.getHost(),
+                        mongo.getMappedPort(27017))
+        );
+        
+        
+     // Use a test database
+        database = client.getDatabase("hostel_db");
+        database.drop(); // ensure clean start
+
+        // Use a collection for apartments
+        apartmentCollection = database.getCollection("apartments");
+    }
+    
+    @After
+    public void tearDown() {
+        // Close MongoDB connection after test
+        if (client != null) {
+            client.close();
+        }
+    }
+
+
+    @Test
+    public void simpleTestToCheckConnectivity() {
+    	
+    }
+    
+}
