a4b1176b8993cbe982826997b85898df7dc18742
SyedBilal61 <bilalkazmi61.bk@gmail.com>
2025-11-12 12:22:46 +0100

    Modifications

--- src/main/java/hostel_management/Main.java
+++ src/main/java/hostel_management/Main.java
@@ -1,6 +1,7 @@
 package hostel_management;
 
 import org.bson.Document;
+
 import com.mongodb.MongoClient;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
@@ -9,34 +10,22 @@ import com.mongodb.client.MongoDatabase;
  * Simple app accessing MongoDB.
  */
 public class Main {
-    public static void main(String[] args) {
-        // Default host is localhost
-        String mongoHost = "mongodb"; //or local host if not using docker 
-        if (args.length > 0) {
+	public static void main(String[] args) {
+        String mongoHost = "localhost";//or mongoDB when running through docker
+        if (args.length > 0)
             mongoHost = args[0];
-        }
-        
-        MongoClient mongoClient = null;
-        
-        try {
-            // Default MongoDB port is 27017
-            mongoClient = new MongoClient(mongoHost);
-            MongoDatabase db = mongoClient.getDatabase("mydb");
-            MongoCollection<Document> collection = db.getCollection("examples");
-            // Create a simple document
-            Document doc = new Document("name", "Greeting")
-                        .append("type", "HelloWorld!");
-            collection.insertOne(doc);
 
-        } catch (Exception e) {
-        	// This makes sure the test passes even if MongoDB is not running
-        	System.out.println("MongoDB not Available" + e.getMessage());
-        } finally {
-        	if (mongoClient != null) {
-        		mongoClient.close();
-        	}
-        	}
-        }
-        
-        
+        // Default port for MongoDB is 27017
+        MongoClient mongoClient = new MongoClient(mongoHost);
+        MongoDatabase db = mongoClient.getDatabase("mydb");
+        MongoCollection<Document> collection = db.getCollection("examples");
+
+        Document doc = new Document("name", "Greeting")
+                .append("type", "HelloWorld!");
+        collection.insertOne(doc);
+
+        // Should print "HelloWorld!"
+        System.out.println(collection.find().first().get("type"));
+        mongoClient.close();
+    }
 }
