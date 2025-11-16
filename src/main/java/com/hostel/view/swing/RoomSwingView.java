85bac314b4d9710bf6451192b31dd203300ed922
SyedBilal61 <bilalkazmi61.bk@gmail.com>
2025-11-16 08:21:33 +0100

    following TDD make thee UI Test Pass

--- src/main/java/com/hostel/view/swing/RoomSwingView.java
+++ src/main/java/com/hostel/view/swing/RoomSwingView.java
@@ -1,42 +1,113 @@
 package com.hostel.view.swing;
 
-import java.awt.GridBagConstraints;
-import java.awt.GridBagLayout;
-import java.awt.Insets;
-
 import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JTextField;
 
 import com.hostel.view.RoomView;
+import java.awt.GridBagLayout;
+import javax.swing.JLabel;
+import java.awt.GridBagConstraints;
+import javax.swing.JTextField;
+import java.awt.Insets;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.JList;
+import javax.swing.ListSelectionModel;
+import javax.swing.JScrollPane;
+
 
 public class RoomSwingView extends JFrame implements RoomView {
-	private JTextField txtRoomidtextbox;
+	private JTextField txtId;
+	private JTextField txtName;
 	public RoomSwingView() {
+		setTitle("HostelView");
 		GridBagLayout gridBagLayout = new GridBagLayout();
 		gridBagLayout.columnWidths = new int[]{0, 0, 0};
-		gridBagLayout.rowHeights = new int[]{0, 0};
+		gridBagLayout.rowHeights = new int[]{0, 0, 0, 0, 0, 0, 0};
 		gridBagLayout.columnWeights = new double[]{0.0, 1.0, Double.MIN_VALUE};
-		gridBagLayout.rowWeights = new double[]{0.0, Double.MIN_VALUE};
+		gridBagLayout.rowWeights = new double[]{0.0, 0.0, 0.0, 1.0, 0.0, 0.0, Double.MIN_VALUE};
 		getContentPane().setLayout(gridBagLayout);
 		
 		JLabel lblNewLabel = new JLabel("RoomId");
 		GridBagConstraints gbc_lblNewLabel = new GridBagConstraints();
-		gbc_lblNewLabel.insets = new Insets(0, 0, 0, 5);
+		gbc_lblNewLabel.insets = new Insets(0, 0, 5, 5);
 		gbc_lblNewLabel.anchor = GridBagConstraints.EAST;
 		gbc_lblNewLabel.gridx = 0;
 		gbc_lblNewLabel.gridy = 0;
 		getContentPane().add(lblNewLabel, gbc_lblNewLabel);
 		
-		txtRoomidtextbox = new JTextField();
-		txtRoomidtextbox.setName("roomIdTextBox");
-		txtRoomidtextbox.setText("roomIdTextBox");
-		GridBagConstraints gbc_txtRoomidtextbox = new GridBagConstraints();
-		gbc_txtRoomidtextbox.fill = GridBagConstraints.HORIZONTAL;
-		gbc_txtRoomidtextbox.gridx = 1;
-		gbc_txtRoomidtextbox.gridy = 0;
-		getContentPane().add(txtRoomidtextbox, gbc_txtRoomidtextbox);
-		txtRoomidtextbox.setColumns(10);
+		txtId = new JTextField();
+		txtId.setName("roomIdTextBox");
+		GridBagConstraints gbc_textField = new GridBagConstraints();
+		gbc_textField.insets = new Insets(0, 0, 5, 0);
+		gbc_textField.fill = GridBagConstraints.HORIZONTAL;
+		gbc_textField.gridx = 1;
+		gbc_textField.gridy = 0;
+		getContentPane().add(txtId, gbc_textField);
+		txtId.setColumns(10);
+		
+		JLabel lblNewLabel_1 = new JLabel("Tenant Name");
+		GridBagConstraints gbc_lblNewLabel_1 = new GridBagConstraints();
+		gbc_lblNewLabel_1.anchor = GridBagConstraints.NORTHEAST;
+		gbc_lblNewLabel_1.insets = new Insets(0, 0, 5, 5);
+		gbc_lblNewLabel_1.gridx = 0;
+		gbc_lblNewLabel_1.gridy = 1;
+		getContentPane().add(lblNewLabel_1, gbc_lblNewLabel_1);
+		
+		txtName = new JTextField();
+		txtName.setName("nameTextBox");
+		GridBagConstraints gbc_textField_1 = new GridBagConstraints();
+		gbc_textField_1.insets = new Insets(0, 0, 5, 0);
+		gbc_textField_1.fill = GridBagConstraints.HORIZONTAL;
+		gbc_textField_1.gridx = 1;
+		gbc_textField_1.gridy = 1;
+		getContentPane().add(txtName, gbc_textField_1);
+		txtName.setColumns(10);
+		
+		JButton btnNewButton = new JButton("Add");
+		btnNewButton.setEnabled(false);
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		GridBagConstraints gbc_btnNewButton = new GridBagConstraints();
+		gbc_btnNewButton.insets = new Insets(0, 0, 5, 0);
+		gbc_btnNewButton.gridwidth = 2;
+		gbc_btnNewButton.gridx = 0;
+		gbc_btnNewButton.gridy = 2;
+		getContentPane().add(btnNewButton, gbc_btnNewButton);
+		
+		JScrollPane scrollPane = new JScrollPane();
+		GridBagConstraints gbc_scrollPane = new GridBagConstraints();
+		gbc_scrollPane.insets = new Insets(0, 0, 5, 0);
+		gbc_scrollPane.fill = GridBagConstraints.BOTH;
+		gbc_scrollPane.gridwidth = 2;
+		gbc_scrollPane.gridx = 0;
+		gbc_scrollPane.gridy = 3;
+		getContentPane().add(scrollPane, gbc_scrollPane);
+		
+		JList list = new JList();
+		scrollPane.setViewportView(list);
+		list.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+		list.setName("roomList");
+		
+		JButton btnNewButton_1 = new JButton("Delete Selected");
+		btnNewButton_1.setEnabled(false);
+		GridBagConstraints gbc_btnNewButton_1 = new GridBagConstraints();
+		gbc_btnNewButton_1.insets = new Insets(0, 0, 5, 0);
+		gbc_btnNewButton_1.gridwidth = 2;
+		gbc_btnNewButton_1.gridx = 0;
+		gbc_btnNewButton_1.gridy = 4;
+		getContentPane().add(btnNewButton_1, gbc_btnNewButton_1);
+		
+		JLabel lblNewLabel_2 = new JLabel(" ");
+		lblNewLabel_2.setName("errorMessageLabel");
+		GridBagConstraints gbc_lblNewLabel_2 = new GridBagConstraints();
+		gbc_lblNewLabel_2.gridwidth = 2;
+		gbc_lblNewLabel_2.insets = new Insets(0, 0, 0, 5);
+		gbc_lblNewLabel_2.gridx = 0;
+		gbc_lblNewLabel_2.gridy = 5;
+		getContentPane().add(lblNewLabel_2, gbc_lblNewLabel_2);
 	}
-
+	
 }
